8bafe158b62f9d6ab09d3298922125e47d7ba540
Hongbin Miao <zslrmhb@gmail.com>
2024-02-25 21:53:11 -0800

    update github pages

--- docs/_app/immutable/chunks/paths.MJlz0oG7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1h3f4dw?.base??"",a=globalThis.__sveltekit_1h3f4dw?.assets??s;export{a,s as b};
--- docs/_app/immutable/entry/start.NNsr5ak8.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.1JRniXi4.js";export{t as start};
