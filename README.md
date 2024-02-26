f9f5b207976651a4d63c7d716dc6ff4843074f3a
Hongbin Miao <zslrmhb@gmail.com>
2024-02-26 12:40:38 -0800

    update github pages

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# Three-Body Problem  
+# Three-Body Problem
+
 A Visual Explorable Inspired by [Matrix Explorable](https://github.com/yizhe-ang/matrix-explorable/tree/55173898d062e40d90a63f5ee6148a02d6031c63)
 
-Made with: 
+## Tools
+
+Template
 - The Pudding's [Svelte Starter Template](https://github.com/the-pudding/svelte-starter)
+
+3D Visualization and Physics Engine
 - [Threlte](https://threlte.xyz/)
+
+
+Math Presentation
 - [Mathbox](https://github.com/unconed/mathbox)
+- [Katex](https://katex.org)
+
+
+
+UI
+- [Skeleton](https://www.skeleton.dev)
+
+Animation
+- [GSAP](https://greensock.com/gsap/)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
+// @ts-check
+import { join } from "path";
+
+// 1. Import the Skeleton plugin
+import { skeleton } from "@skeletonlabs/tw-plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: "class",
+	content: [
+		"./src/**/*.{html,js,svelte,ts}",
+		// 3. Append the path to the Skeleton package
+		join(
+			require.resolve("@skeletonlabs/skeleton"),
+			"../**/*.{html,js,svelte,ts}"
+		)
+	],
 	theme: {
 		extend: {}
 	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ["dark"]
-	}
+	plugins: [
+		skeleton({
+			themes: { preset: ["skeleton"] }
+		})
+	]
 };
-
